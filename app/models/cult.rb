5782385c2e65960e8d7c2a623717ec6ca68e2156
MullaN <admullan@gmail.com>
2020-08-28 15:30:58 -0700

    pair programming 082820

---
+++ app/models/cult.rb
@@ -0,0 +1,42 @@
+class Cult
+    attr_accessor :name, :location, :founding_year, :slogan
+
+    @@all = []
+
+    def initialize(name, location, founding_year, slogan)
+        @name = name
+        @location = location
+        @founding_year = founding_year
+        @slogan = slogan
+
+        @@all << self
+    end
+
+    def recruit_follower(follower)
+        BloodOath.new("date", self, follower)
+    end
+
+    def oaths
+        BloodOath.all.select {|oath| oath.cult == self}
+    end
+
+    def cult_population
+        self.followers.length
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_name(name)
+        self.all.find {|cult| cult.name == name}
+    end
+
+    def self.find_by_location(location)
+        self.all.select {|cult| cult.location == location}
+    end
+
+    def self.find_by_founding_year(year)
+        self.all.select {|cult| cult.founding_year == year}
+    end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,8 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Cult.new("Cult 1", "Random Town", 2020, "This is our slogan")
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
